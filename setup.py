c6faf02ba2b937db045fb1008cc0a35034f785d0
Charles Montgomery <chazzofalf@gmail.com>
2023-03-02 23:45:36 -0600

    Added Setup.py for installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='hiddenrec',
+      version='0.0',
+      description='Hidden Sound Recorder/Player',
+      author='Charles Montgomery',
+      author_email='charles.montgomery@charter.net',
+      url='https://github.com/chazzofalf/hiddenrec',
+      packages=['hiddenrec']      
+     )
